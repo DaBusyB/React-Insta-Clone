5be45164334cec402516626d415886d1959c0f0d
Brandi Apetsi <Brandi@Brandis-iMac.local>
2019-03-13 22:17:03 -0400

    basic component and file setup

---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './CommentSection.css'
+
+function CommentSection {
+    return (
+        <div>
+            <h1>CommentSection</h1>
+        </div>
+    )
+}
+
+export default CommentSection;
---
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './PostContainer.css'
+import CommentSection from '../CommentSection/CommentSection';
+
+function PostContainer {
+    return (
+        <div>
+            <h1>PostContainer</h1>
+        </div>
+    )
+}
+
+export default PostContainer;
